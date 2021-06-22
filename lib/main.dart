cdb92105d667568c607ef4bc7573c066b65a6e6b
alkxyly <alkxyly@hotmail.com>
2021-06-22 12:03:50 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:playflow/modules/home/home_page.dart';
+import 'package:playflow/modules/login/login_page.dart';
+import 'package:playflow/modules/splash/splash_page.dart';
+import 'package:playflow/shared/themes/AppColors.dart';
+
+void main() {
+  runApp(AppWidget());
+}
+
+class AppWidget extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Play Flow',
+      theme: ThemeData(
+      primaryColor: AppColors.primary),
+      home: const LoginPage()
+    );
+  }
+}
+
---
+++ lib/modules/splash/splash_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:playflow/shared/themes/AppColors.dart';
+import 'package:playflow/shared/themes/app_images.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      body: Stack(
+        children: [
+          Center(child: Image.asset(AppImages.union)),
+          Center(child: Image.asset(AppImages.logoFull))
+        ],
+      ),
+    );
+  }
+}
